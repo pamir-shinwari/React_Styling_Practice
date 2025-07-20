9f4617a8e8b1c43c23ad769257170e229b526bff
Pamir Shinwari <32494542+pamir-shinwari@users.noreply.github.com>
2025-07-20 17:27:43 +0000

    adding style

--- src/index.js
+++ src/index.js
@@ -18,12 +18,19 @@ const hours_int = parseInt(hours);
 
 var message = "";
 
+const color_style = {
+  color: "",
+};
+
 if (hours_int > 23 && hours_int < 12) {
   message = "Good Morning";
+  color_style.color = "red";
 } else if (hours_int >= 12 && hours_int < 18) {
   message = "Good Afternoon";
+  color_style.color = "green";
 } else if (hours_int >= 18 && hours_int < 24) {
   message = "Good Afternoon";
+  color_style.color = "blue";
 }
 
-root.render(<h1>{message}</h1>);
+root.render(<h1 style={color_style}>{message}</h1>);
